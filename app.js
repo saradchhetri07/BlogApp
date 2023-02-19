5027473e5de33b9ecac574ab13aa471c3c2c5859
saradchhetri07 <saradchhetri20690@gmail.com>
2023-02-19 07:06:39 +0545

    Mongodb database has been added and hosted over the cloud

--- app.js
+++ app.js
@@ -4,11 +4,37 @@ const date = require(__dirname + "/date.js");
 const bodyParser = require("body-parser");
 var _ = require("lodash");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+//mongoose database
+const mongoose = require("mongoose");
 
-var titleList = [];
-var blogList = [];
-var total = "";
+main().catch((err) => console.log(err));
+//mongodb+srv://saradChhetri:<password>@atlascluster.x8spv7w.mongodb.net/?retryWrites=true&w=majority
+
+async function main() {
+  const username = "saradChhetri";
+  const password = encodeURIComponent("Lu77pa@7882");
+
+  await mongoose.connect(
+    "mongodb+srv://" +
+      username +
+      ":" +
+      password +
+      "@atlascluster.x8spv7w.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  );
+}
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  date: String,
+  blog: String,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -17,39 +43,94 @@ app.set("view engine", "ejs");
 app.listen(3000, function (req, res) {
   console.log("server is running at port 3000");
 });
+var titleList = [];
+var blogList = [];
+var total = "";
+var dateList = [];
 
 app.post("/compose", function (req, res) {
   var title = req.body.title.toUpperCase();
-  var blog = req.body.blogPost;
+  var blogFromCompose = req.body.blogPost;
 
-  titleList.push(title);
-  blogList.push(blog);
+  var day = date.getDate();
+  var time = date.getTime();
+  total = day + "," + time;
+
+  const newBlog = new Blog({
+    title: title,
+    blog: blogFromCompose,
+    date: total,
+  });
+
+  newBlog.save();
 
   res.redirect("/");
 });
+
 app.get("/", function (req, res) {
   var day = date.getDate();
   var time = date.getTime();
   total = day + "," + time;
-  res.render("home", {
-    listTitle: titleList,
-    postBlog: blogList,
-    newDay: total,
-  });
+  try {
+    Blog.find({}, function (err, foundBlogs) {
+      if (err) {
+        console.log(err);
+      } else {
+        for (var i = 0; i < foundBlogs.length; i++) {
+          titleList.push(foundBlogs[i].title);
+          blogList.push(foundBlogs[i].blog);
+          dateList.push(foundBlogs[i].date);
+        }
+      }
+      console.log(titleList.length);
+      res.render("home", {
+        listTitle: titleList,
+        postBlog: blogList,
+        newDay: dateList,
+      });
+      titleList = [];
+      blogList = [];
+      dateList = [];
+    });
+  } catch (err) {
+    console.log(error);
+  }
+});
+app.get("/new", function (req, res) {
+  res.render("new");
 });
 app.get("/post/:postTitle", function (req, res) {
-  for (var i = 0; i < titleList.length; i++) {
-    if (
-      _.lowerCase([(string = req.params.postTitle)]) ===
-      titleList[i].toLowerCase()
-    ) {
-      res.render("post", {
-        listTitle: titleList[i],
-        postBlog: blogList[i],
-        newDay: total,
-      });
+  Blog.find({}, function (err, foundBlogs) {
+    if (err) {
+      console.log(err);
+    } else {
+      for (var i = 0; i < foundBlogs.length; i++) {
+        if (
+          _.lowerCase([(string = req.params.postTitle)]) ===
+          foundBlogs[i].title.toLowerCase()
+        ) {
+          res.render("post", {
+            listTitle: foundBlogs[i].title,
+            postBlog: foundBlogs[i].blog,
+            newDay: foundBlogs[i].date,
+            foundBlogs: foundBlogs[i],
+          });
+        }
+      }
     }
-  }
+  });
+  // for (var i = 0; i < titleList.length; i++) {
+  //   if (
+  //     _.lowerCase([(string = req.params.postTitle)]) ===
+  //     titleList[i].toLowerCase()
+  //   ) {
+  //     res.render("post", {
+  //       listTitle: titleList[i],
+  //       postBlog: blogList[i],
+  //       newDay: dateList[i],
+  //     });
+  //   }
+  // }
 });
 app.get("/aboutUs", function (req, res) {
   res.render("aboutUs");
@@ -59,4 +140,15 @@ app.get("/contactUs", function (req, res) {
 });
 app.get("/compose", function (req, res) {
   res.render("compose");
+});
+app.post("/delete", function (req, res) {
+  console.log(req.body);
+  Blog.findByIdAndRemove(req.body.item, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("delete by ID");
+    }
+  });
+  res.redirect("/");
 });
